afd6ce15febbb36988dbcee0f6ff303ebc4c1c3a
natalie-o446 <odelln8790@my.sduhsd.net>
2025-04-08 09:22:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Add JavaScript code for your web site here and call it from index.html.
+document.getElementById("myBtn")?.addEventListener("click",() => {
+    let input = document.getElementById("myInput").value;
+    document.getElementById("myPara").innerHTML = "you typed: " + input;
+});
 
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+  }
